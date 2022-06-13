0c8835210f4a6e02e8e9b543587b725bf0d0f08f
LongNguyen_DDC <25nguyenduclong@gmail.com>
2022-06-13 20:24:30 +0700

    add post push patch detele

--- server.js
+++ server.js
@@ -4,9 +4,28 @@ const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+    req.body.updateAt = Date.now()
+
+  }
+  // Continue to JSON Server router
+  next()
+})
+
 server.use(middlewares);
 server.use('/api',router);
 
 server.listen(port);
-
-
