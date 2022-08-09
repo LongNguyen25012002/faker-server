948beeb3c91308390e39e7ef9ba986c956c0e26c
LongNguyen_DDC <25nguyenduclong@gmail.com>
2022-08-09 12:39:48 +0700

    creaet menu api

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now()
     req.body.updateAt = Date.now()
-
   }
   // Continue to JSON Server router
   next()
